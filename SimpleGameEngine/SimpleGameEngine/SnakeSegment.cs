2222534e0eb0204355e6eff920e9de2ef62555e0
Supersonic101101 <55442156+Supersonic101101@users.noreply.github.com>
2019-09-18 09:52:54 +0200

    Revert "Merge branch 'complete-snake-segment'"
    This reverts commit b8f211dec2327a30969b4b4ca47cc579aa317cd7, reversing
    changes made to 2de60008d34d40bdeb6272b45f8be139d69ba164.

--- SimpleGameEngine/SimpleGameEngine/SnakeSegment.cs
+++ SimpleGameEngine/SimpleGameEngine/SnakeSegment.cs
@@ -7,25 +7,35 @@ public class SnakeSegment{
     private char character; //the character representing this snake segment visually
 
     public SnakeSegment(int x, int y, char character){
-        this.x = x;
-        this.y = y;
-        this.character = character;
-
+        /*  ------------------------------------------
+            1.1
+            ------------------------------------------   
+            Initialize the x, y and character fields 
+            using the values passed to the constructor
+        */
     }
 
     public int X {
         get {return x;}
         set {
-            prevX = x;
-            x = value;
+            /*  ------------------------------------------
+                1.2
+                ------------------------------------------
+                Before overwriting x with the new value,
+                set prevX to the current value of x
+            */
         }
     }
 
     public int Y {
         get {return y;}
         set {
-            prevY = y;
-            y = value;
+            /*  ------------------------------------------
+                1.3
+                ------------------------------------------
+                Before overwriting y with the new value,
+                set prevY to the current value of y
+            */
         }
     }
 
